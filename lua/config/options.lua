c1d14841d66a045fb49b2b3e2ef37b5609871886
Damien <damienfuster31@gmail.com>
2025-07-18 09:09:21 +0100

    ajout vimtex début

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -14,6 +14,20 @@ vim.opt.showmode = false -- Pas de double indication
 vim.opt.number = true
 vim.opt.relativenumber = true
 vim.opt.cursorline = true
+
 -- Minimal number of screen lines to keep above and below the cursor.
 vim.opt.scrolloff = 5
+vim.opt.sidescrolloff = 8  
+
+vim.opt.softtabstop = 2 -- Soft tab stop 
+
+vim.opt.termguicolors = true                       -- Enable 24-bit colors
+vim.opt.signcolumn = "yes"                         -- Always show sign column
+--vim.opt.colorcolumn = "120"      -- Show column at 100 characters
+vim.opt.cmdheight = 1 -- Command line height
+
+vim.opt.backup = false                             -- Don't create backup files
+vim.opt.writebackup = false                        -- Don't create backup before writing
+vim.opt.swapfile = false                           -- Don't create swap files
+
 
