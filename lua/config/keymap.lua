532b45ccf1e52fcd78bb4098730eed088eea764f
damien <damienfuster31@gmail.com>
2026-06-25 07:26:12 +0200

    Changement du thème en noir

--- lua/config/keymap.lua
+++ lua/config/keymap.lua
@@ -1,3 +1,4 @@
 vim.keymap.set("n", "-", "<cmd>Oil --float<CR>", {desc="Ouvre le fichier parent avec le package Oil"})
 vim.keymap.set("n", ")", "<cmd>write | !python3 %<CR>")
+vim.keymap.set("n", "à", "<cmd>write | !Rscript %<CR>")
 
--- lua/plugins/dashboard.lua
+++ lua/plugins/dashboard.lua
@@ -1,11 +1,16 @@
 return {
-     "goolord/alpha-nvim",
-     config = function ()
-         local alpha = require'alpha'
-         local dashboard = require'alpha.themes.dashboard'
-         dashboard.section.header.val = {
-               
+    "goolord/alpha-nvim",
+    config = function()
+        local alpha = require("alpha")
+        local dashboard = require("alpha.themes.dashboard")
 
+        -- Couleur du header (ASCII)
+        vim.api.nvim_set_hl(0, "AlphaHeader", {
+            fg = "#000000",
+            bg = "NONE",
+        })
+
+        dashboard.section.header.val = {
 [[                                      ..:::.                         ]],
 [[                                   .:#@@@@@@@*.                      ]],
 [[                                 .+@@@@@###%+@@:                     ]],
@@ -33,22 +38,25 @@ return {
 [[ |  (_,_)\  |\  `-'    /\ `"/  \  ) / \ (_,_) /  |   ||  (_,_)  |  | ]],
 [[ |  |    |  | \       /  '. \_/``".'   \     /   |   ||  |      |  | ]],
 [[ '--'    '--'  `'-..-'     '-----'      `---`    '---''--'      '--' ]],
-         
-         }
-         dashboard.section.buttons.val = {
-             dashboard.button( "e", "  New file" , ":ene <BAR> startinsert <CR>"),
-             dashboard.button( "q", "󰅚  Quit NVIM" , ":qa<CR>"),
-         }
-         local handle = io.popen('fortune')
-         local fortune = handle:read("*a")
-         handle:close()
-         dashboard.section.footer.val = fortune
+        }
 
-         dashboard.config.opts.noautocmd = true
+        dashboard.section.header.opts.hl = "AlphaHeader"
 
-         vim.cmd[[autocmd User AlphaReady echo 'ready']]
+        dashboard.section.buttons.val = {
+            dashboard.button("e", "  New file", ":ene <BAR> startinsert <CR>"),
+            dashboard.button("q", "󰅚  Quit NVIM", ":qa<CR>"),
+        }
 
-         alpha.setup(dashboard.config)
-     end
- }
+        local handle = io.popen("fortune")
+        local fortune = handle:read("*a")
+        handle:close()
 
+        dashboard.section.footer.val = fortune
+
+        dashboard.config.opts.noautocmd = true
+
+        vim.cmd([[autocmd User AlphaReady echo 'ready']])
+
+        alpha.setup(dashboard.config)
+    end,
+}
